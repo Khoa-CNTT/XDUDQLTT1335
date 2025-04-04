b0a42ba42032bf5996788101682c16c894dbb5e0
huynhphuoc1206 <huynhphuoc12062003@gmail.com>
2025-04-05 00:09:08 +0700

    project ver 0504

--- DuAnEnglish/Controllers/HomeAdminController.cs
+++ DuAnEnglish/Controllers/HomeAdminController.cs
@@ -11,6 +11,26 @@ namespace DuAnEnglish.Controllers
         // GET: HomeAdmin
         public ActionResult Index()
         {
+            //// Kiểm tra xem session có lưu đúng giá trị role không
+            //if (Session["User"] != null)
+            //{
+            //    System.Diagnostics.Debug.WriteLine("User in session: " + Session["User"]);
+            //}
+            //else
+            //{
+            //    System.Diagnostics.Debug.WriteLine("No user found in session.");
+            //}
+
+            //if (Session["Role"] != null)
+            //{
+            //    System.Diagnostics.Debug.WriteLine("Role in session: " + Session["Role"]);
+            //}
+            //else
+            //{
+            //    System.Diagnostics.Debug.WriteLine("No role found in session.");
+            //}
+
+            // Còn lại phần xử lý logic của HomeAdmin nếu cần
             return View();
         }
     }
